4192b46cccb9afe293b1d494a707a163d986c663
Vathys <deepdesai00@gmail.com>
2026-01-21 16:49:17 -0600

    fix: Enable manual node setup in publish.yml

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  basePath: "/vathys.github.com"
+  basePath: "/vathys.github.com",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
